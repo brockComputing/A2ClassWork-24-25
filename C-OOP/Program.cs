030ff71611786ece3d3f4e1b16e71010cde19e43
Charlie Brown <cbrown@brock.ac.uk>
2024-09-10 16:24:00 +0100

    C and D oop

---
+++ C-OOP/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_OOP
+{
+    class Employee
+    {
+        private string name;
+        private string title;
+        protected double rate;
+        public string addresss;
+
+        public Employee(string name)
+        {
+            this.name = name;
+        }
+
+        public Employee()
+        {
+            name = "ttttttttt";
+        }
+
+        public virtual void UpdateRate(double percentAmount)
+        {
+            rate = (percentAmount / 100 * rate) + rate;
+        }
+        public void SetName(string thename)
+        {
+            name = thename;
+        }
+        public double GetRate()
+        {
+            return rate;
+        }
+
+        public string Getname()
+        {
+            return name;
+        }
+
+        public string getTitle()
+        {
+            return title;
+        }
+    }
+
+    class Grunt : Employee
+    {
+        public string colourOfOveralls;
+        public override void UpdateRate(double percentAmount)
+        {
+            base.UpdateRate(percentAmount);
+        }
+        public void SetOverallColour(string theColour)
+        {
+            rate = 45.7;
+            
+            colourOfOveralls = theColour;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee("beb");
+            Employee emp2 = new Employee("dave");
+            Employee emp3 = new Employee();
+            Employee  emp4 = new Grunt();
+            
+            emp.SetName("ted");
+        }
+    }
+}
---
+++ D-OOP/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_OOP
+{
+    class Employee
+    {
+        private string name;
+        private string title;
+        private double rate;
+
+        public Employee(string title)
+        {
+            this.title = title;
+        }
+        public Employee()
+        {
+            name = "xxxxxx";
+        }
+
+        public void UpdateRate(double percentAmount)
+        {
+            rate = (percentAmount / 100 * rate) + rate;
+        }
+
+        public double GetRate()
+        {
+            return rate;
+        }
+        public void SetName(string theName)
+        {
+            name = theName;
+        }
+        public string Getname()
+        {
+            return name;
+        }
+
+        public string getTitle()
+        {
+            return title;
+        }
+
+    }
+
+    class Cleaner : Employee
+    {
+        private int noMops;
+        public int GetNoMops()
+        {
+            return noMops;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee("mr");
+            Employee emp2 = new Employee();
+            Cleaner cln1 = new Cleaner();
+           
+            Console.WriteLine(emp2.Getname());
+            emp1.SetName("ted");
+            
+
+        }
+    }
+}
